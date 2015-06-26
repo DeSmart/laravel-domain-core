6b9776cc31b95c6a836dd33985a90c58e97de081
Radoslaw Mejer <radmen@desmart.com>
2015-06-26 14:50:36 +0200

    removed abstract entities and their specs/stubs

--- stubs/Entity/EloquentEntityStub.php
+++
@@ -1,8 +0,0 @@
-<?php namespace DeSmart\DomainCore\Stubs\Entity;
-
-use DeSmart\DomainCore\Entity\AbstractEloquentEntity;
-
-class EloquentEntityStub extends AbstractEloquentEntity
-{
-    // this stub should stay empty.. 
-}
--- stubs/Entity/UserStubEntity.php
+++
@@ -1,7 +0,0 @@
-<?php namespace DeSmart\DomainCore\Stubs\Entity;
-
-use DeSmart\DomainCore\Entity\AbstractEloquentEntity;
-
-class UserStubEntity extends AbstractEloquentEntity
-{
-}
