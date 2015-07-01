2e2eb140ee38d269e25a37ff13c12f9cf555a192
Kamil Piechaczek <pomek@desmart.com>
2015-07-01 14:15:50 +0200

    Clean Service Provider

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -10,25 +10,5 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
     public function register()
     {
         $this->app->bind('DeSmart\DomainCore\EntityTranslatorInterface', 'DeSmart\DomainCore\EntityTranslator');
-
-        $this->app->bind(
-            'DeSmart\DomainCore\Commands\Contracts\CommandBus',
-            'DeSmart\DomainCore\Commands\CommandBus'
-        );
-
-        $this->app->bind(
-            'DeSmart\DomainCore\Commands\Factory\Contracts\HandlerLocator',
-            'DeSmart\DomainCore\Commands\Factory\HandlerLocatorFactory'
-        );
-
-        $this->app->bind(
-            'DeSmart\DomainCore\Commands\Factory\Contracts\ValidatorLocator',
-            'DeSmart\DomainCore\Commands\Factory\ValidatorLocatorFactory'
-        );
-
-        $this->app->bind(
-            'DeSmart\DomainCore\Commands\Extractor\Contracts\CommandNameExtractor',
-            'DeSmart\DomainCore\Commands\Extractor\ClassNameExtractor'
-        );
     }
 }
